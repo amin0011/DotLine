e8ed20e4a09668d86ea65e08792c87fa15569d36
amin0011 <tomsklen25@gmail.com>
2020-09-15 06:06:47 -0700

    change-main

--- app/src/main/java/project/activity/MainActivity.java
+++ app/src/main/java/project/activity/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends EnhancedActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    //  dsdasd
+    //amin ui
 
     requestWritePermission();
 
